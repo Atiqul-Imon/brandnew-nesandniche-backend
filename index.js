0e52e6ab213c0df1869b5d1476ee64219502082f
atiqul-islam <imonatikulislam@gmail.com>
2025-06-29 22:21:11 +0600

    First deploy

--- index.js
+++ index.js
@@ -30,7 +30,13 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://www.newsandniche.com',
+    'https://newsandniche.com'
+  ],
+  credentials: true
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
