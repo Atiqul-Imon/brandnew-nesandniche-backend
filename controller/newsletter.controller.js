15df72f31fcf586bb46415f1293a5c52bc33bd36
atiqul-islam <imonatikulislam@gmail.com>
2025-07-11 17:14:19 +0600

    fix: use default import for logger in newsletter.controller.js

--- controller/newsletter.controller.js
+++ controller/newsletter.controller.js
@@ -1,5 +1,5 @@
 import Newsletter from '../model/newsletter.model.js';
-import { logger } from '../utils/logger.js';
+import logger from '../utils/logger.js';
 import { sendEmail } from '../utils/emailService.js';
 
 // Subscribe to newsletter
